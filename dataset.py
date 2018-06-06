4db018b56653f2e7f2715b13b1242af8da241211
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-06 13:33:36 +0200

    Add print function

--- dataset.py
+++ dataset.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 def read_data():
     return pd.read_csv('./data/datalog-05102017-Full_1Hz.1s_clean.csv', sep=';')
+
+def print_data(df, steps):
+    i = 0
+    while i<len(df.keys()):
+        print('{}\n'.format(df.iloc[:, i:i+steps]))
+        i+=(steps+1)
