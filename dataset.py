00ef40cf67ee706a376db8d5c6f2be89c5398492
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-06 16:31:37 +0200

    Create correlation matrix

--- dataset.py
+++ dataset.py
@@ -1,15 +1,30 @@
-import math
-import pandas as pd
-
 def read_data():
+    import pandas as pd
+
     return pd.read_csv('./data/datalog-05102017-Full_1Hz.1s_clean.csv', sep=';')
 
 def find_corr_of(df, target):
-    for col in df.drop(['date TU', 'heure TU', 'latitude', 'longitude'], axis=1).keys():
+    import math
+
+    for col in df.drop(['date TU', 'heure TU', 'latitude', 'longitude'], axis=1).columns:
         if col != target:
             corr = df[target].corr(df[col])
             if not math.isnan(corr):
                 yield (col, corr)
+
+def plot_correlation_matrix(df):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import seaborn as sns
+    
+    _, ax = plt.subplots(figsize=(14, 10))
+    corr = df.corr()
+    sns.heatmap(corr,
+                mask=np.zeros_like(corr, dtype=np.bool),
+                cmap=sns.diverging_palette(220, 10, as_cmap=True),
+                square=True,
+                ax=ax)
+    plt.show()
 
 # Read from CSV.
 df = read_data()
@@ -26,3 +41,6 @@ for col, corr in sorted(list(find_corr_of(df, 'WTP_SelBoatSpd')),
                         key=lambda x: x[1],
                         reverse=True):
     print('{}: {}'.format(col, corr))
+
+# Plot the correlation matrix.
+plot_correlation_matrix(df)
