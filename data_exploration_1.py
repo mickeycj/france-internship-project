bc367322ff75a51b43a7aef64bce4c865d85c2b4
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-08 13:53:59 +0200

    Finish data exploration #1

--- data_exploration_1.py
+++ data_exploration_1.py
@@ -1,7 +1,7 @@
-def read_data():
-    import pandas as pd
-
-    return pd.read_csv('./data/datalog-05102017-Full_1Hz.1s_clean.csv', sep=';')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 
 def find_corr_of(df, target):
     import math
@@ -12,29 +12,15 @@ def find_corr_of(df, target):
             if not math.isnan(corr):
                 yield (col, corr)
 
-def plot_correlation_matrix(df):
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import seaborn as sns
-    
-    _, ax = plt.subplots(figsize=(14, 10))
-    corr = df.corr()
-    sns.heatmap(corr,
-                mask=np.zeros_like(corr, dtype=np.bool),
-                cmap=sns.diverging_palette(220, 10, as_cmap=True),
-                square=True,
-                ax=ax)
-    plt.show()
-
 # Read from CSV.
-df = read_data()
+df = pd.read_csv('./data/datalog-05102017-Full_1Hz.1s_clean.csv', sep=';')
 
 # Show the sample.
 sample = df.head(n=10)
 i = 0
 while i<len(sample.keys()):
     print('{}\n'.format(sample.iloc[:, i:i+5]))
-    i+=6
+    i+=5
 
 # Find the correlation between the speed of the boat with different sensors.
 for col, corr in sorted(list(find_corr_of(df, 'WTP_SelBoatSpd')),
@@ -43,4 +29,11 @@ for col, corr in sorted(list(find_corr_of(df, 'WTP_SelBoatSpd')),
     print('{}: {}'.format(col, corr))
 
 # Plot the correlation matrix.
-plot_correlation_matrix(df)
+_, ax = plt.subplots(figsize=(14, 10))
+corr = df.corr()
+sns.heatmap(corr,
+            mask=np.zeros_like(corr, dtype=np.bool),
+            cmap=sns.diverging_palette(220, 10, as_cmap=True),
+            square=True,
+            ax=ax)
+plt.show()
