b037cd0475b003b21c6a9b93a8127a8e2db33638
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-11 16:10:28 +0200

    Save bins to PDF

--- plot_bins.py
+++ plot_bins.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 import pandas as pd
 import sys, utils
 
@@ -12,15 +13,18 @@ wind_features = ['WTP_AW_angle', 'WTP_AW_speed']
 # Combine positive and negative wind angle.
 df[wind_features[0]] = df[wind_features[0]].apply(lambda x: abs(x))
 
-# Plot the bins.
+# Plot and save the bins.
 fig = plt.figure()
 ax = fig.gca()
+plt.xlabel(wind_features[0])
+plt.ylabel(wind_features[1])
+plt.plot(df[wind_features[0]].tolist(), df[wind_features[1]].tolist(), 'ko', markersize=0.25)
 ax.set_xticks(np.arange(0, 181, 20))
 ax.set_xticks(np.arange(0, 181, 10), minor=True)
 ax.set_yticks(np.arange(0, 53, 4))
 ax.set_yticks(np.arange(0, 53, 2), minor=True)
-plt.xlabel(wind_features[0])
-plt.ylabel(wind_features[1])
 plt.grid(which='both')
-plt.plot(df[wind_features[0]].tolist(), df[wind_features[1]].tolist(), 'ko', markersize=0.25)
-plt.show()
+path = './report'
+if not os.path.exists(path):
+    os.makedirs(path)
+plt.savefig('{}/bins.pdf'.format(path))
