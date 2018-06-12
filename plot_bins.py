635cd6b2d32029a65b4d4eb39e3912497d2642f9
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-12 18:14:15 +0200

    Adjust bin thresholds

--- plot_bins.py
+++ plot_bins.py
@@ -11,19 +11,25 @@ plt.rcParams.update({'figure.max_open_warning': 0})
 wind_features = ['WTP_AW_angle', 'WTP_AW_speed']
 
 # Create bins.
-def create_bins(df, x_steps=10, y_steps=2, outlier_thresh=1, min_thresh=5):
+def create_bins(df, x_steps=10, y_steps=2, outlier_thresh=10, min_thresh=100):
     bins = {}
     x, y = 0, 0
-    while y <= 52-y_steps:
-        while x <= 180-x_steps:
+    while x <= 180:
+        while y <= 52:
             binned_df = df.query('{0} >= {2} and {0} < {2}+{4} & {1} >= {3} & {1} < {3}+{5}'.format(wind_features[0], wind_features[1], x, y, x_steps, y_steps))
-            if len(binned_df) > outlier_thresh:
+            if len(binned_df) >= outlier_thresh:
                 if len(binned_df) < min_thresh:
                     return create_bins(df, x_steps+2, y_steps+1)
-                bins['bin_x{}to{}_y{}to{}'.format(x, x+x_steps, y, y+y_steps)] = binned_df
-            x = x+x_steps
-        x = 0
-        y = y+y_steps
+                x_end = x+x_steps
+                if x_end > 180:
+                    x_end = 180
+                y_end = y+y_steps
+                if y_end > 52:
+                    y_end = 52
+                bins['bin_x{}to{}_y{}to{}'.format(x, x_end, y, y_end)] = binned_df
+            y = y+y_steps
+        y = 0
+        x = x+x_steps
     return bins, x_steps, y_steps
 
 # Plot angle-speed space.
@@ -33,10 +39,8 @@ def plot_angle_speed(df, x_start, y_start, x_finish, y_finish, x_steps, y_steps,
     plt.xlabel(wind_features[0])
     plt.ylabel(wind_features[1])
     plt.plot(df[wind_features[0]].tolist(), df[wind_features[1]].tolist(), 'ko', markersize=markersize)
-    ax.set_xticks(np.arange(x_start, x_finish, x_steps*2))
-    ax.set_xticks(np.arange(x_start, x_finish, x_steps), minor=True)
-    ax.set_yticks(np.arange(y_start, y_finish, y_steps*2))
-    ax.set_yticks(np.arange(y_start, y_finish, y_steps), minor=True)
+    ax.set_xticks(np.arange(x_start, x_finish, x_steps))
+    ax.set_yticks(np.arange(y_start, y_finish, y_steps))
     plt.grid(which='both')
     if not os.path.exists(base_path):
         os.makedirs(base_path)
@@ -57,5 +61,5 @@ base_path = './report'
 plot_angle_speed(df, 0, 0, 181, 53, x_steps, y_steps, 0.25, base_path, 'all_bins')
 for bin_name, df in bins.items():
     x_start, x_finish, y_start, y_finish = [int(s) for s in re.findall(r'\d+', bin_name)]
-    dx, dy = (x_finish-x_start)/8.0, (y_finish-y_start)/8.0
+    dx, dy = (x_finish-x_start)/4.0, (y_finish-y_start)/4.0
     plot_angle_speed(df, x_start, y_start, x_finish+dx, y_finish+dy, dx, dy, 3, base_path, bin_name)
