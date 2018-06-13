6af8ed7a0184c0f6b9006c85ac4a60c206099566
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-13 14:32:23 +0200

    Update variable names

--- plot_bins.py
+++ plot_bins.py
@@ -12,21 +12,20 @@ wind_features = ['WTP_AW_angle', 'WTP_AW_speed']
 # Create bins.
 def create_bins(df, dx=10, dy=2, outlier_thresh=10, min_thresh=100):
     bins = {}
-    x, y = 0, 0
-    while x <= 180:
-        y = 0
-        while y <= 52:
-            binned_df = df.query('{0} >= {2} and {0} < {2}+{4} & {1} >= {3} & {1} < {3}+{5}'.format(wind_features[0], wind_features[1], x, y, dx, dy))
+    max_x, max_y = 0, 0
+    while max_x <= 180:
+        max_y = 0
+        while max_y <= 52:
+            binned_df = df.query('{0} >= {2} and {0} < {2}+{4} & {1} >= {3} & {1} < {3}+{5}'.format(wind_features[0], wind_features[1], max_x, max_y, dx, dy))
             if len(binned_df) >= outlier_thresh:
                 if len(binned_df) < min_thresh:
                     return create_bins(df, dx+2, dy+1)
-                x_end, y_end = x+dx, y+dy
-                bins['bin_x{}to{}_y{}to{}'.format(x, x_end, y, y_end)] = binned_df
-            y = y+dy
-        x = x+dx
-    if x > 180:
-        x = 180
-    return bins, dx, dy, x, y
+                bins['bin_x{}to{}_y{}to{}'.format(max_x, max_x+dx, max_y, max_y+dy)] = binned_df
+            max_y+=dy
+        max_x+=dx
+    if max_x > 180:
+        max_x = 180
+    return bins, dx, dy, max_x, max_y
 
 # Plot angle-speed space.
 def plot_angle_speed(df, x_start, y_start, x_finish, y_finish, dx, dy, markersize, base_path, fname):
@@ -50,11 +49,11 @@ df = utils.read_csv('{}.csv'.format(sys.argv[1]))
 df.eval('{0} = abs({0})'.format(wind_features[0]), inplace=True)
 
 # Determine the size of the bins.
-bins, dx, dy, x_max, y_max = create_bins(df)
+bins, dx, dy, max_x, max_y = create_bins(df)
 
 # Plot and save the bins.
 base_path = './report'
-plot_angle_speed(df, 0, 0, x_max+1, y_max+1, dx, dy, 0.25, base_path, 'bins')
+plot_angle_speed(df, 0, 0, max_x+1, max_y+1, dx, dy, 0.25, base_path, 'bins')
 for bin_name, df in bins.items():
     x_start, x_finish, y_start, y_finish = [int(s) for s in re.findall(r'\d+', bin_name)]
     dx, dy = (x_finish-x_start)/4.0, (y_finish-y_start)/4.0
