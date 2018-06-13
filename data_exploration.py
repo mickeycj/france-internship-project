3429d9658d0beafcebc99b879b397beb8bc33b55
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-13 14:15:13 +0200

    Uncomment data exploration example

--- data_exploration.py
+++ data_exploration.py
@@ -80,26 +80,26 @@ def plot_corr(df, fname):
                 square=True,
                 ax=ax).get_figure().savefig('{}/{}.pdf'.format(path,fname))
 
-# # Example
-# # Read from CSV.
-# df = utils.read_csv('{}.csv'.format(sys.argv[1]))
+# Example
+# Read from CSV.
+df = utils.read_csv('{}.csv'.format(sys.argv[1]))
 
-# # The column names to be reduced/combined.
-# fname = sys.argv[2]
-# if fname == 'max_min':
-#     cols = max_min_cols
-# elif fname == 'avg':
-#     cols = max_min_cols
-# else:
-#     print('Invalid file name!')
-#     exit()
+# The column names to be reduced/combined.
+fname = sys.argv[2]
+if fname == 'max_min':
+    cols = max_min_cols
+elif fname == 'avg':
+    cols = max_min_cols
+else:
+    print('Invalid file name!')
+    exit()
 
-# # Reduce different sensors from the same location to the same columns and print the result.
-# transformed_df = transform(df.copy(deep=True), cols, '{}.*')
-# # utils.print_data(transformed_df.head(10), 7)
+# Reduce different sensors from the same location to the same columns and print the result.
+transformed_df = transform(df.copy(deep=True), cols, '{}.*')
+# utils.print_data(transformed_df.head(10), 7)
 
-# # Find the correlation between the speed of the boat with different sensors.
-# write_corr(find_corr_of(transformed_df, 'WTP_SelBoatSpd', exclude=['date TU', 'heure TU', 'latitude', 'longitude']), '{}_corr'.format(fname))
+# Find the correlation between the speed of the boat with different sensors.
+write_corr(find_corr_of(transformed_df, 'WTP_SelBoatSpd', exclude=['date TU', 'heure TU', 'latitude', 'longitude']), '{}_corr'.format(fname))
 
-# # Plot the correlation matrix.
-# plot_corr(transformed_df, '{}_corr'.format(fname))
+# Plot the correlation matrix.
+plot_corr(transformed_df, '{}_corr'.format(fname))
