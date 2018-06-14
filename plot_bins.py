8efba12ca582f2949dfe55c89aef320d0994c94d
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-15 01:17:45 +0200

    Remove redundant parameter - outlier_thresh

--- plot_bins.py
+++ plot_bins.py
@@ -14,16 +14,16 @@ wind_features = ['WTP_AW_angle', 'WTP_AW_speed']
 axis_names = ['Wind Angle (˚)', 'Wind Speed (knot)']
 
 # Create bins.
-def create_bins(df, dx=bin_angles[0], dy=2, outlier_thresh=10, min_thresh=100, tries=0):
+def create_bins(df, dx=bin_angles[0], dy=2, min_thresh=100, tries=0):
     bins = {}
     max_x, max_y = 0, 0
     while max_x < 180:
         max_y = 0
         while max_y < math.ceil(df.loc[df[wind_features[1]].idxmax()][wind_features[1]]):
             binned_df = df.query('{0} >= {2} and {0} < {2}+{4} & {1} >= {3} & {1} < {3}+{5}'.format(wind_features[0], wind_features[1], max_x, max_y, dx, dy))
-            if len(binned_df) >= outlier_thresh:
+            if len(binned_df) >= int(math.ceil(min_thresh * 0.1)):
                 if len(binned_df) < min_thresh:
-                    return create_bins(df, dx=bin_angles[tries+1], dy=dy+2, outlier_thresh=outlier_thresh, min_thresh=min_thresh, tries=tries+1)
+                    return create_bins(df, dx=bin_angles[tries+1], dy=dy+2, min_thresh=min_thresh, tries=tries+1)
                 bins['bin_x{}to{}_y{}to{}'.format(max_x, max_x+dx, max_y, max_y+dy)] = binned_df
             max_y+=dy
         max_x+=dx
@@ -52,13 +52,13 @@ df = utils.read_csv('{}.csv'.format(sys.argv[1]))
 df.eval('{0} = abs({0})'.format(wind_features[0]), inplace=True)
 
 # Create different-sized bins.
-bin_properties = [(1, 10), (3, 50), (10, 100)]
-for outlier_thresh, min_thresh in bin_properties:
+bin_sizes = [10, 50, 100]
+for min_thresh in bin_sizes:
     # Determine the size of the bins.
-    bins, dx, dy, max_x, max_y = create_bins(df, outlier_thresh=outlier_thresh, min_thresh=min_thresh)
+    bins, dx, dy, max_x, max_y = create_bins(df, min_thresh=min_thresh)
 
     # Plot and save the bins.
-    base_path = './report/outlier{}_min{}'.format(outlier_thresh, min_thresh)
+    base_path = './report/min_thresh_{}'.format(min_thresh)
     plot_angle_speed(df, 0, 0, max_x+1, max_y+1, dx, dy, 0.25, base_path, 'bins')
     for bin_name, binned_df in bins.items():
         x_start, x_finish, y_start, y_finish = [int(s) for s in re.findall(r'\d+', bin_name)]
