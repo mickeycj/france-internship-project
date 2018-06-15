be4b306730aac9849f8269090565f3c5040f8e85
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-15 11:35:49 +0200

    Fix wrong boxplot's axis name

--- plot_bins.py
+++ plot_bins.py
@@ -10,9 +10,10 @@ plt.rcParams.update({'figure.max_open_warning': 0})
 bin_angles = [4, 5, 6, 9, 10, 12, 15, 18, 20, 30, 45]
 
 # List of relevant features and axis names.
-boat_speed = 'WTP_SelBoatSpd'
 wind_features = ['WTP_AW_angle', 'WTP_AW_speed']
 axis_names = ['Wind Angle (˚)', 'Wind Speed (knot)']
+boxplot_feature = 'WTP_SelBoatSpd'
+boxplot_axis_name = 'Boat Speed (knot)'
 
 # Create directory if not exist.
 def create_if_not_exist(path):
@@ -53,7 +54,9 @@ def plot_angle_speed(df, x_start, y_start, x_finish, y_finish, dx, dy, markersiz
 # Plot boxplot for speed.
 def plot_boxplot(df, column, outliers, base_path, fname):
     df.boxplot(column=column, showfliers=outliers)
-    plt.xlabel(column)
+    plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
+    plt.ylabel(boxplot_axis_name)
+    plt.tight_layout()
     create_if_not_exist(base_path)
     plt.savefig('{}/{}.pdf'.format(base_path, fname))
     plt.clf()
@@ -79,4 +82,4 @@ for min_thresh in bin_sizes:
         x_start, x_finish, y_start, y_finish = [int(s) for s in re.findall(r'\d+', bin_name)]
         dx, dy = (x_finish-x_start)/4.0, (y_finish-y_start)/4.0
         plot_angle_speed(binned_df, x_start, y_start, x_finish+dx, y_finish+dy, dx, dy, 3, bin_base_path, 'bin')
-        plot_boxplot(binned_df, boat_speed, False, bin_base_path, 'boxplot')
+        plot_boxplot(binned_df, boxplot_feature, False, bin_base_path, 'boxplot')
