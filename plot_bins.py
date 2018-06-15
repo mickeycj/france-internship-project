1e525e993105e4c7ce7210f9e6222a43b26e274e
Chanon Jenakom <chanonjenakom@gmail.com>
2018-06-15 11:20:54 +0200

    Create speed boxplot for each bin

--- plot_bins.py
+++ plot_bins.py
@@ -10,8 +10,14 @@ plt.rcParams.update({'figure.max_open_warning': 0})
 bin_angles = [4, 5, 6, 9, 10, 12, 15, 18, 20, 30, 45]
 
 # List of relevant features and axis names.
+boat_speed = 'WTP_SelBoatSpd'
 wind_features = ['WTP_AW_angle', 'WTP_AW_speed']
 axis_names = ['Wind Angle (˚)', 'Wind Speed (knot)']
+
+# Create directory if not exist.
+def create_if_not_exist(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 # Create bins.
 def create_bins(df, dx=bin_angles[0], dy=2, min_thresh=100, tries=0):
@@ -30,7 +36,7 @@ def create_bins(df, dx=bin_angles[0], dy=2, min_thresh=100, tries=0):
     return bins, dx, dy, max_x, max_y
 
 # Plot angle-speed space.
-def plot_angle_speed(df, x_start, y_start, x_finish, y_finish, dx, dy, markersize, base_path, fname,):
+def plot_angle_speed(df, x_start, y_start, x_finish, y_finish, dx, dy, markersize, base_path, fname):
     fig = plt.figure()
     ax = fig.gca()
     plt.xlabel(axis_names[0])
@@ -40,8 +46,14 @@ def plot_angle_speed(df, x_start, y_start, x_finish, y_finish, dx, dy, markersiz
     ax.set_yticks(np.arange(y_start, y_finish, dy))
     plt.grid(lw=.75)
     plt.tight_layout()
-    if not os.path.exists(base_path):
-        os.makedirs(base_path)
+    create_if_not_exist(base_path)
+    plt.savefig('{}/{}.pdf'.format(base_path, fname))
+    plt.clf()
+
+# Plot boxplot for speed.
+def plot_boxplot(df, column, outliers, base_path, fname):
+    df.boxplot(column=column, showfliers=outliers)
+    create_if_not_exist(base_path)
     plt.savefig('{}/{}.pdf'.format(base_path, fname))
     plt.clf()
 
@@ -61,6 +73,8 @@ for min_thresh in bin_sizes:
     base_path = './report/min_thresh_{}'.format(min_thresh)
     plot_angle_speed(df, 0, 0, max_x+1, max_y+1, dx, dy, 0.25, base_path, 'bins')
     for bin_name, binned_df in bins.items():
+        bin_base_path = '{}/{}'.format(base_path, bin_name)
         x_start, x_finish, y_start, y_finish = [int(s) for s in re.findall(r'\d+', bin_name)]
         dx, dy = (x_finish-x_start)/4.0, (y_finish-y_start)/4.0
-        plot_angle_speed(binned_df, x_start, y_start, x_finish+dx, y_finish+dy, dx, dy, 3, base_path, bin_name)
+        plot_angle_speed(binned_df, x_start, y_start, x_finish+dx, y_finish+dy, dx, dy, 3, bin_base_path, 'bin')
+        plot_boxplot(binned_df, boat_speed, False, bin_base_path, 'boxplot')
